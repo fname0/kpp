13a7d62af34f2aff8ba000c07847a2e434ac5389
fname0 <kmailfw@mail.ru>
2023-05-29 11:14:52 +0300

    c

--- pages/zaSvoboduHipHopaParashutHlopnul.js
+++ pages/zaSvoboduHipHopaParashutHlopnul.js
@@ -6,20 +6,20 @@ export default function ZaSvoboduHipHopaParashutHlopnul() {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-    axios.get(`http://95.174.102.106:7474/getorders.php`)
+    axios.get(`https://db-lovat.vercel.app/api//getorders.php`)
         .then(res => {
             setOrders(res.data);
         })
     }, [])
 
     const deleteOrder = (id) => {
-        axios.get(`http://95.174.102.106:7474/deleteorder.php?id=`+id);
+        axios.get(`https://db-lovat.vercel.app/api//deleteorder.php?id=`+id);
         delete orders[id];
         setOrders(orders);
     }
 
     const deleteOrderRepair = (id, rawId) => {
-        axios.get(`http://95.174.102.106:7474/deleterepair.php?id=`+id);
+        axios.get(`https://db-lovat.vercel.app/api//deleterepair.php?id=`+id);
         delete orders[rawId];
         setOrders(orders);
     }
