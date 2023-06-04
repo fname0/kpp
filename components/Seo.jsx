dce9040b7bc655bbde55a7d357229839d613a3ee
fname0 <kmailfw@mail.ru>
2023-06-04 12:08:19 +0300

    c

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -5,7 +5,7 @@ const Seo = ({title, keywords, description}) => {
     <Head>
         <title>{title}</title>
         <meta property="og:title" content={title} />
-        <meta property="og:image" content="/kamazTestAva.png" />
+        <meta property="og:image" content="https://kpp116.ru/favicon.ico" type="image/x-icon"/>
         <meta name='description' content={description}/>
         <meta property="og:description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
