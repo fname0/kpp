a5e9f8019167152ac7690ac31722e4dddbccff18
fname0 <kmailfw@mail.ru>
2023-06-04 12:10:01 +0300

    c

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -5,13 +5,13 @@ const Seo = ({title, keywords, description}) => {
     <Head>
         <title>{title}</title>
         <meta property="og:title" content={title} />
-        <meta property="og:image" content="https://kpp116.ru/favicon.ico" type="image/x-icon"/>
+        <meta property="og:image" content="/kamazTestAva.png" />
         <meta name='description' content={description}/>
         <meta property="og:description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <meta name="keywords" content={keywords} />
         <meta charSet="utf-8"></meta>
-        <link rel="icon" href="https://kpp116.ru/kamazTestAvaMin.png"/>
+        <link rel="icon" href="https://kpp116.ru/favicon.ico" type="image/x-icon"/>
         <link rel="apple-touch-icon" href="/favicon.ico"/>
     </Head>
      );
