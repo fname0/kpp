a5d76815e2608c4a691d5c0032fd4bceaf2e1d40
fname0 <kmailfw@mail.ru>
2023-06-13 11:35:10 +0300

    c

--- api/deleterepair.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-require_once __DIR__ . "/vendor/autoload.php";
-class_alias("\RedBeanPHP\R", "\R");
-R::setup( 'mysql:host=95.174.102.106;dbname=kamaz', 'remote', 'yaDumalPass');
-$order = R::findOne("repair", "WHERE id = ?", [$_GET['id']]);
-$order->completed = true;
-R::store($order);
-?>
