f4720b708abc512551af4165fe1a00b5d863ba7e
fname0 <kmailfw@mail.ru>
2023-06-14 21:19:26 +0300

    c

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
     const response = NextResponse.next()
