3e09b0826e815cd8777c81269e7c2ad5feb803ef
fname0 <kmailfw@mail.ru>
2023-06-14 21:22:44 +0300

    c

--- pages/api/setproductinfo.js
+++ pages/api/setproductinfo.js
@@ -1,5 +1,13 @@
 export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
     console.log(req)
-    res.headers.append("Access-Control-Allow-Origin", "*")
     res.status(200).json({ name: 'John Doe' })
 }
