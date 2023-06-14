7e7ddd2445ea857f1b58d66f652f05b2ebe30bab
fname0 <kmailfw@mail.ru>
2023-06-14 21:04:01 +0300

    c

--- pages/setproductinfo.js
+++ pages/setproductinfo.js
@@ -1,3 +1,10 @@
 export default function handler(req, res) {
     console.log(req)
+    return new Response('Hello, Next.js!', {
+        status: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+        }
+      });
 }
