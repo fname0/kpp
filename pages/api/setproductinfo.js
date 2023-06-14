453be087dcaed6123ec0d2f511646140d16cbef1
fname0 <kmailfw@mail.ru>
2023-06-15 00:19:52 +0300

    c

--- pages/api/setproductinfo.js
+++ pages/api/setproductinfo.js
@@ -1,10 +1,7 @@
 export default function handler(req, res) {
     fetch("http://95.174.102.106:7474/api/test.php", {
       method: "POST",
-      body: req.body,
-      headers: {
-        "content-type": "application/json",
-      },
+      body: req.body
     });
     res.status(200).send(req.body);
 }
