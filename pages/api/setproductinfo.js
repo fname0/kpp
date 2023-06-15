4df2ade8623b9304c473891a4e1fb54720349c98
fname0 <kmailfw@mail.ru>
2023-06-15 10:10:54 +0300

    c

--- pages/api/setproductinfo.js
+++ pages/api/setproductinfo.js
@@ -1,3 +1,9 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
 export default function handler(req, res) {
     fetch("http://95.174.102.106:7474/api/test.php", {
       method: "POST",
