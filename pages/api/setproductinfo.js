3c0bf7f0d84f096140a933b25eff7748b03a6f50
fname0 <kmailfw@mail.ru>
2023-06-15 10:08:22 +0300

    c

--- pages/api/setproductinfo.js
+++ pages/api/setproductinfo.js
@@ -1,8 +1,13 @@
-export async function handler(req) {
-  const data = await req.json();
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+export default function handler(req, res) {
     fetch("http://95.174.102.106:7474/api/test.php", {
       method: "POST",
-      body: data
+      body: req.body
     });
-    return new Response('fff');
+    res.status(200).send(req.body);
 }
