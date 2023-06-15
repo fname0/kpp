17b1773d151f6c97d8453b99be8edeed3fdb6eb8
fname0 <kmailfw@mail.ru>
2023-06-15 09:57:12 +0300

    c

--- pages/api/setproductinfo.js
+++ pages/api/setproductinfo.js
@@ -3,5 +3,5 @@ export default function handler(req, res) {
       method: "POST",
       body: req.body
     });
-    res.status(200).send(req.body);
+    res.status(200).send(JSON.stringify(req.body));
 }
