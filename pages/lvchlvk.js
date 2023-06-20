7162463ab5d58644764eaf86136460b7cf559063
fname0 <kmailfw@mail.ru>
2023-06-20 15:24:13 +0300

    с

--- pages/lvchlvk.js
+++ pages/lvchlvk.js
@@ -12,14 +12,12 @@ export default function Lvchlvk() {
 
     const doSomething = function (e) {
         const formData = new FormData();
-        console.log(e);
-        console.log({"Редуктор": "reductor", "КПП" :"kpp", "Сцепление(+кулиса)": "scepa", "Метиз(+датчики, РТИ)": "metiz", "Подшипник": "podshib", "ZF": "ZF"}[e.target[3].value]);
         formData.append('title', e.target[0].value);
         formData.append('num', e.target[1].value);
         formData.append('price', e.target[2].value);
         formData.append('cat', {"Редуктор": "reductor", "КПП" :"kpp", "Сцепление(+кулиса)": "scepa", "Метиз(+датчики, РТИ)": "metiz", "Подшипник": "podshib", "ZF": "ZF"}[e.target[3].value]);
         formData.append('out', e.target[4].checked); 
-        // axios.post(`https://db-lovat.vercel.app/api/addproduct.php`, formData)
+        axios.post(`https://db-lovat.vercel.app/api/addproduct.php`, formData)
         // window.location.href = "/greetings";
         e.preventDefault();
     }
