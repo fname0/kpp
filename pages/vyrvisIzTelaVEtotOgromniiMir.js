a9369acf39df547e7bda633618048ad11fbce4fd
fname0 <kmailfw@mail.ru>
2023-06-20 16:04:44 +0300

    c

--- pages/vyrvisIzTelaVEtotOgromniiMir.js
+++ pages/vyrvisIzTelaVEtotOgromniiMir.js
@@ -25,7 +25,7 @@ export default class vyrvisIzTelaVEtotOgromniiMir extends React.Component {
   }
 
   deleteProduct() {
-    axios.get(`https://db-lovat.vercel.app/api/deleteproduct.php?id=`+new URLSearchParams(window.location.search).get('id'));
+    axios.get(`https://db-lovat.vercel.app/api/productdelete.php?id=`+new URLSearchParams(window.location.search).get('id'));
   }
 
   render() {
