0d9645bd76b1f679b30ff5002b82016f968a6ae5
fname0 <kmailfw@mail.ru>
2023-06-20 15:07:41 +0300

    c

--- pages/lvchlvk.js
+++ pages/lvchlvk.js
@@ -39,15 +39,16 @@ export default function Lvchlvk() {
                         <p className="buyFormText">Цена:</p>
                         <input type="text" className="buyFormInput"/>
                     </label>
-                    <select id="cars" name="cars" size="2" multiple>
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
-                        <option value="saab">Saab</option>
-                        <option value="fiat">Fiat</option>
-                        <option value="audi">Audi</option>
-                    </select>
+                    <label>
+                        <input list="browsers" className="buyFormInput"/>
+                        <datalist id="browsers">
+                            <option value="Internet Explorer">
+                            <option value="Firefox">
+                            <option value="Chrome">
+                            <option value="Opera">
+                            <option value="Safari">
+                        </datalist>
+                    </label>
                     <div>
                         <input type="checkbox" name="out" id="out" className="buyCheckbox"/>
                         <label htmlFor="agree" className="buyCheckboxText">В наличии</label>
