141f24e3fba8bf21e7ca829f5fe446078d30de50
fname0 <kmailfw@mail.ru>
2023-06-27 21:30:59 +0300

    c

--- pages/api/getproduct.js
+++ pages/api/getproduct.js
@@ -1,5 +1,15 @@
 export default async function handler(req, res) {
     const query = req.query;
     const { id } = query;
-    res.status(200).json({prodId: id})
+
+    const dbconnection = await mysql.createConnection({
+        host: "95.174.102.106",
+        database: "kamaz",
+        user: "remote",
+        password: "yaDumalPass"
+    });
+    const [data] = await dbconnection.execute("SELECT * FROM products WHERE id = ?", id);
+    dbconnection.end();
+
+    res.status(200).json(data)
 }
