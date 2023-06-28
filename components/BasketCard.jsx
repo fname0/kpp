553b0668884d7525cde0adea6a7560de94ffae59
fname0 <kmailfw@mail.ru>
2023-06-28 18:08:59 +0300

    c

--- components/BasketCard.jsx
+++ components/BasketCard.jsx
@@ -21,12 +21,12 @@ function BasketCard(props) {
     }
 
     return (
-        <div className={deleted === "0" ? "basketCard" : "basketCardDel"}>
+        <div className={props.deleted === "0" ? "basketCard" : "basketCardDel"}>
             <img src={tryRequire()} alt="" className="basketCardImg"/>
             <p className="basketCardTitle">{props.title}</p>
             <p className="basketCardNum">{props.num}</p>
-            {deleted === "0" ? <p className="basketCardTitle">{props.price}₽{parseInt(count) === 1 ? null : "(" + props.price*count + ")"}</p> : null }
-            {deleted === "0" ? <div className="basketCountInputCont"><input type="text" className="basketCountInput" value={count} onChange={(event) => {setCount(event.target.value); countChange(event.target.value)}}/></div> :
+            {props.deleted === "0" ? <p className="basketCardTitle">{props.price}₽{parseInt(count) === 1 ? null : "(" + props.price*count + ")"}</p> : null }
+            {props.deleted === "0" ? <div className="basketCountInputCont"><input type="text" className="basketCountInput" value={count} onChange={(event) => {setCount(event.target.value); countChange(event.target.value)}}/></div> :
             <p className="basketCardTitle">товар был удалён</p>
             }
             <img src="/imgs/trash.svg" alt="" className="basketCardDelete" onClick={() => props.delete(props.id)}/>
