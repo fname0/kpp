baed3840c21604dfa146576157b6375221d09a30
fname0 <kmailfw@mail.ru>
2023-06-28 15:50:43 +0300

    c

--- pages/etoPretPizheKrekaPodnimaiNashFlag.js
+++ pages/etoPretPizheKrekaPodnimaiNashFlag.js
@@ -4,9 +4,9 @@ import Image from 'next/image';
 export default function AdminProducts() {
   return (
     <div className="App">
-        <div className="categoryCont">
         <Image src='/imgs/kamazHeader.jpg' placeholder="blur" sizes="100vw" fill quality={100} style={{objectFit: "cover"}}/>
 
+        <div className="categoryCont">
             <h1 className="homeHeader">Админка</h1>
             <AdminCards cat={"all"}/>
         </div>
