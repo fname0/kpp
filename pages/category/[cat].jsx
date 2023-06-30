4eb0bd163621c5173cecb4460e017dc0babdb678
fname0 <kmailfw@mail.ru>
2023-06-30 20:45:38 +0300

    c

--- pages/category/[cat].jsx
+++ pages/category/[cat].jsx
@@ -28,7 +28,7 @@ export default function ({products}) {
   }, [])
 
   useEffect(() => {
-    console.log(productsToRender.length);
+    console.log(productsToRender);
     if (fetching) {
       axios.get(`https://db-lovat.vercel.app/api/?cat=`+cat+`&start=`+toString(productsToRender.length)+`&limit=`+toString(productsToRender.length+32))
       .then(res => {
