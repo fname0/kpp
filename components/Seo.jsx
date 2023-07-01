e751b6451d17efd25fdcf3a2c5f05df2a3eaa638
fname0 <kmailfw@mail.ru>
2023-07-01 11:30:33 +0300

    c

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -4,6 +4,7 @@ const Seo = ({title, keywords, description}) => {
     return ( 
     <Head>
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, shrink-to-fit=no"></meta>
         <meta property="og:title" content={title} />
         <meta property="og:image" content="/kamazTestAva.png" />
         <meta name='description' content={description}/>
