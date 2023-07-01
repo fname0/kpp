5a8477eba948ef02223df6df15a07c4c8898afc3
fname0 <kmailfw@mail.ru>
2023-07-01 12:35:37 +0300

    c

--- pages/category/[cat].jsx
+++ pages/category/[cat].jsx
@@ -60,7 +60,8 @@ export default function ({products}) {
 
   const scrollHandler = (e) => {
     if (Object.keys(products).length <= step) return;
-    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < {10} && productsToRender[Object.keys(productsToRender)[Object.keys(productsToRender).length-1]].id !== products[Object.keys(products)[Object.keys(products).length-1]].id)
+    k = step === 32 ? 3 : 50;
+    if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < k && productsToRender[Object.keys(productsToRender)[Object.keys(productsToRender).length-1]].id !== products[Object.keys(products)[Object.keys(products).length-1]].id)
     {
       // setContHeight(document.getElementById('productCardCont').scrollHeight);
       setFetching(1);
