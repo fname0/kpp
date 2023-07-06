1c7512ac5193b2e2ecf38594bd7adf4e01fe9e76
fname0 <kmailfw@mail.ru>
2023-07-06 11:44:37 +0300

    c

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -38,7 +38,7 @@ export default function ({product}) {
     return (
         <div className="App">
             <Seo title={product.title+'('+product.num+') за '+ product.price+'₽'} description={'Купить '+product.title+'('+product.num+')'+' за '+product.price+"₽"} keywords={"запчасти камаз, купить запчасти камаз, "+product.title+", "+product.num+", "+{"reductor": "Редукторы", "kpp": "КПП", "scepa": "Сцепление(+кулиса)", "metiz": "Метизы(+датчики, РТИ)", "podshib": "Подшипники", "ZF": "ZF"}[product.cat]}/>
-            <Image src='/imgs/kamazHeader.jpg' placeholder="blur" sizes="100vw" fill quality={100} style={{objectFit: "cover"}} onClick={() => {setIsBigImgOpened(true)}}/>
+            <Image src='/imgs/kamazHeader.jpg' placeholder="blur" sizes="100vw" fill quality={100} style={{objectFit: "cover"}}/>
 
             <button className="contactsFloatingBtn"><Link href="/">
                 <img src='/imgs/home.svg' alt="" className="contactsFloatingBtnImg"/>
@@ -53,7 +53,7 @@ export default function ({product}) {
             <div className="productInfoCont">
               {product.out === undefined ? product.out = "False" : null}
                 <div className="productInfoImgCont">
-                    <Image src={tryRequire()} placeholder="blur" alt="" className="productInfoCardImg"/>
+                    <Image src={tryRequire()} placeholder="blur" alt="" className="productInfoCardImg" onClick={() => {setIsBigImgOpened(true);console.log("tt")}}/>
                 </div>
                 <h1 className="homeHeader">{product.title}</h1>
                 <h1 className="homeHeader">{product.num}</h1>
